2b24a4893347521b97b884253afde3a660537e8f
sakthivel0420 <123206266+sakthivel0420@users.noreply.github.com>
2023-01-21 01:39:58 +0530

    Initial commit

---
+++ src/com/TestNG/TestAPI.java
@@ -0,0 +1,45 @@
+package com.TestNG;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+public class TestAPI {
+	
+	    WebDriver driver;
+
+	
+	    public void setUp() {
+	        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+	        driver = new ChromeDriver();
+	    }
+
+	   
+	    public void testJSONResponse() {
+	        driver.get("https://api.coindesk.com/v1/bpi/currentprice.json");
+	        String response = driver.findElement(By.tagName("pre")).getText();
+
+	        JsonParser parser = new JsonParser();
+	        JsonObject json = parser.parse(response).getAsJsonObject();
+
+	        String time = json.get("time").getAsJsonObject().get("updated").getAsString();
+	        String rate = json.get("bpi").getAsJsonObject().get("USD").getAsJsonObject().get("rate").getAsString();
+
+	        Assert.assertNotNull(time);
+	        Assert.assertNotNull(rate);
+	    }
+
+	    
+	    public void tearDown() {
+	        driver.quit();
+	    }
+	}
+
+
+
---
+++ src/com/TestNG/TestAdactin.java
@@ -0,0 +1,25 @@
+package com.TestNG;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class TestAdactin {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "")
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+}
+	
+	
